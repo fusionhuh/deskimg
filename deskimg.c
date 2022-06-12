aa60d1eac7dabac127dc65ae3f5449c9250cbc4c
Josh <joshk559@gmail.com>
2022-06-12 17:49:24 -0500

    Added clarifying code comments.

--- deskimg.c
+++ deskimg.c
@@ -12,7 +12,7 @@ gchar* filename;
 static gboolean
 clicked (GtkWidget *window, GdkEventButton *event, gpointer user_data)
 {
-  if (event->button == 1 /*&& event->type == GDK_2BUTTON_PRESS*/)
+  if (event->button == 1 /*&& event->type == GDK_2BUTTON_PRESS*/) // GDK_2BUTTON_PRESS corresponds to double click.
   {
     gint root_x;
     gint root_y;
@@ -22,7 +22,6 @@ clicked (GtkWidget *window, GdkEventButton *event, gpointer user_data)
     gtk_window_begin_move_drag (GTK_WINDOW (window), 1, root_x, root_y, 0);
   }
 }
-
 
 static void
 activate (GtkApplication *app, gpointer user_data)
@@ -34,37 +33,36 @@ activate (GtkApplication *app, gpointer user_data)
 
   g_signal_connect (window, "button-press-event", G_CALLBACK (clicked), NULL);
 
-  gtk_widget_add_events(window, GDK_BUTTON_PRESS_MASK);
+  gtk_widget_add_events(window, GDK_BUTTON_PRESS_MASK);       // Adds click event to window widget.
 
   GtkWidget *image;
-  image = gtk_image_new_from_file (filename);
+  image = gtk_image_new_from_file (filename);                 // Loads the image located at the path specified by the -f option.
 
   GdkScreen *screen;
   GdkVisual *visual;
 
-  gtk_widget_set_app_paintable(window, TRUE);
-  screen = gdk_screen_get_default();
-  visual = gdk_screen_get_rgba_visual(screen);
-
-  if (visual!= NULL && gdk_screen_is_composited(screen))
-  {
-    gtk_widget_set_visual(window, visual);
-  }
+  gtk_widget_set_app_paintable(window, TRUE);                 // Makes the window background
+  screen = gdk_screen_get_default();                          // transparent, allowing view
+  visual = gdk_screen_get_rgba_visual(screen);                // to the desktop.
+  if (visual!= NULL && gdk_screen_is_composited(screen))      //
+  {                                                           //
+    gtk_widget_set_visual(window, visual);                    //
+  }                                                           //
 
   gtk_container_add (GTK_CONTAINER (window), image);
 
-  gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
+  gtk_window_set_decorated (GTK_WINDOW (window), FALSE);      // Removes the border around the window.
 
-  gtk_widget_set_opacity(window, opacity);
+  gtk_widget_set_opacity(window, opacity);                    // Sets the window to the opacity specified by options.
 
-  gtk_window_set_keep_below (GTK_WINDOW (window), TRUE);
+  gtk_window_set_keep_below (GTK_WINDOW (window), TRUE);      // Keeps the window below all other windows on screen.
 
-  gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE);
+  gtk_window_set_skip_taskbar_hint (GTK_WINDOW (window), TRUE); // Removes the taskbar icon that would normally appear.
 
   gtk_widget_show_all (window);
 }
 
-static void
+static void // Unused function to satisfy G_APPLICATION_HANDLES_COMMAND_LINE flag.
 command_line (GtkApplication* app, GApplicationCommandLine* cmdline)
 {
   int p = 0;
@@ -106,11 +104,10 @@ main (int argc, char *argv[])
 
   g_application_set_flags(app, G_APPLICATION_NON_UNIQUE);
 
-  //add main options here
-  g_application_add_main_option_entries (app, options);
+  g_application_add_main_option_entries (app, options);           // Adds the options in the GOptionEntry array to the commmand line parser(?)
 
   g_signal_connect (app, "startup", G_CALLBACK (activate), NULL);
-  g_signal_connect_swapped (app, "command-line", G_CALLBACK (command_line), app);
+  g_signal_connect_swapped (app, "command-line", G_CALLBACK (command_line), app); // Unused callback to satisfy G_APPLICATION_HANDLES_COMMAND_LINE flag.
   g_signal_connect_swapped (app, "handle-local-options", G_CALLBACK (handle_local_options), app);
 
   status = g_application_run (G_APPLICATION (app), argc, argv);
